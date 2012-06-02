2e71fac58224fa899c1452064f5b7841880ed153
Guillaume Dievart <dievartg@gmail.com>
2012-06-02 09:42:32 +0200

    First commit for Autoloader

---
+++ lib/Tlc/Autoloader/Autoloader.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Description of Autoloader
+ *
+ * @author gdievart
+ */
+class Autoloader 
+{
+    
+    /**
+     * Register autoload
+     * 
+     * @return type 
+     */
+    public static function registerAutoload()
+    {
+        return spl_autoload_register(array(__CLASS__, 'loadClass'));
+    }
+   
+    
+    /**
+     * Register autoload
+     * 
+     * @return type 
+     */
+    public static function loadClass($class)
+    {
+        echo $class . "\n";
+        if(strtolower(substr($class, 0, 3)) === 'app') {
+            require_once realpath(__DIR__ . '/../../../') . '/' . strtr($class, '_\\', '//') . '.class.php';
+        } else {
+            require_once realpath(__DIR__ . '/../../') . '/' . strtr($class, '_\\', '//') . '.class.php';
+        }
+    }
+}
