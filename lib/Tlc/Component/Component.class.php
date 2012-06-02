d9b03349c38a07c8680716148fafbb709ff7bc51
Guillaume Dievart <dievartg@gmail.com>
2012-06-02 09:45:13 +0200

    First commit for Component

---
+++ lib/Tlc/Component/Component.class.php
@@ -0,0 +1,48 @@
+<?php
+namespace Tlc\Component;
+
+/**
+ * Description of Component
+ *
+ * @author gdievart
+ */
+abstract class Component 
+{
+    
+    /**
+     * Construct Component
+     * 
+     * @param type $config 
+     */
+    public function __construct($config = null) 
+    {
+        if($config !== null && is_array($config) && isset($this->_requiredFields)) {
+            $this->_checkRequiredFields($config);
+        }
+        
+        $this->_init($config);
+    }
+    
+    
+    /**
+     * Check is required fields is present
+     * 
+     * @param array $config
+     */
+    private function _checkRequiredFields($config) 
+    {
+        foreach($this->_requiredFields as $field) {
+            if(!array_key_exists($field, $config)) {
+                throw new ComponentException("The field '$field' is required", 400);
+            }
+        }
+    }
+    
+    
+    /**
+     * Init component
+     * 
+     * @param type $config 
+     */
+    protected function _init($config) { }
+}
