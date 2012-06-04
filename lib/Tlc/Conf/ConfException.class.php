8e1f911ea542edc0c5245e22b88f79f5cfdd6506
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 17:58:07 +0200

    First commit for ConfException

---
+++ lib/Tlc/Conf/ConfException.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tlc\Conf;
+
+use Tlc\Component\ComponentException;
+
+/**
+ * ConfException
+ *
+ * @author gdievart
+ */
+class ConfException extends ComponentException 
+{
+    
+}
