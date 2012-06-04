7457a00fa7ec4f160fa8a579407d26006c54f7c6
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 10:30:10 +0200

    First commit for RouterException

---
+++ lib/Tlc/Router/RouterException.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tlc\Router;
+
+use Tlc\Component\ComponentException;
+
+/**
+ * Description of RouterException
+ *
+ * @author gdievart
+ */
+class RouterException extends ComponentException 
+{
+    
+}
