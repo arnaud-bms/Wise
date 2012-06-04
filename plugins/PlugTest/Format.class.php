48e0c21cbce527b0934b50dd940e25e32901c084
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 14:38:41 +0200

    First commit to plugin Format

---
+++ plugins/PlugTest/Format.class.php
@@ -0,0 +1,33 @@
+<?php
+namespace PlugTest;
+
+use Tlc\Bootstrap\Bootstrap;
+use Tlc\Plugin\Plugin;
+use Tlc\Conf\Conf;
+
+/**
+ * Description of Cache
+ *
+ * @author gdievart
+ */
+class Format extends Plugin
+{
+    
+    /**
+     * Method call on precall
+     */
+    public function precall() { }
+    
+    
+    /**
+     * Method call on postcall
+     */
+    public function postcall()
+    {
+        echo __METHOD__ . "\n";
+        $format = new \Tlc\Format\Format();
+        Bootstrap::setResponse(
+                $format->formatData(Bootstrap::getFormat(), 
+                Bootstrap::getResponse()));
+    }
+}
