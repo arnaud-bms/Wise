38bdac6f7b66ec50bc4ca591561cad15b0d8dc44
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 10:29:55 +0200

    First commit for AutoloaderException

---
+++ lib/Tlc/Autoloader/AutoloaderException.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tlc\Autoloader;
+
+use Tlc\Component\ComponentException;
+
+/**
+ * Description of AutoloaderException
+ *
+ * @author gdievart
+ */
+class AutoloaderException extends ComponentException 
+{
+    
+}
