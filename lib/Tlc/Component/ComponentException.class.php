f3da4d5b7b482e824e5ed103decd8ce384111e5a
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 11:13:09 +0200

    First commit for ComponentException

---
+++ lib/Tlc/Component/ComponentException.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tlc\Component;
+
+/**
+ * Description of ComponentException
+ *
+ * @author gdievart
+ */
+class ComponentException extends \Exception
+{
+    
+    /**
+     * Format message exception
+     * 
+     * @return string message formatted
+     */
+    public function __toString() 
+    {
+        
+    }
+}
