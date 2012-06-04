d64e447bd57d1d65c781573dc762aeeb16d63e7f
Guillaume Dievart <dievartg@gmail.com>
2012-06-04 10:56:40 +0200

    Add use namespace

--- bootstrap.php
+++ bootstrap.php
@@ -5,19 +5,30 @@
  * @author gdievart
  */
 
+use Tlc\Autoloader\Autoloader;
+use Tlc\Conf\Conf;
+use Tlc\Bootstrap\Bootstrap;
+
 define('ROOT_DIR', __DIR__ . '/');
 
 require_once ROOT_DIR . '/lib/Tlc/Autoloader/Autoloader.class.php';
-spl_autoload_register(array('\Tlc\Autoloader\Autoloader', 'loadClass'));
+spl_autoload_register(array('Tlc\Autoloader\Autoloader', 'loadClass'));
 
-\Tlc\Conf\Conf::loadConfig(ROOT_DIR . 'etc/tlc.ini');
+Autoloader::setAlias(array(
+    'Tlc'           => ROOT_DIR . 'lib',
+    'Test'          => ROOT_DIR . 'app',
+    'PlugTest'      => ROOT_DIR . 'plugins'
+));
 
-if($handlerConfig = \Tlc\Conf\Conf::getConfig('exception_handler')) {
+Conf::loadConfig(ROOT_DIR . 'etc/tlc.ini');
+Conf::mergeConfig(ROOT_DIR . 'etc/routing.ini');
+
+if($handlerConfig = Conf::getConfig('exception_handler')) {
     set_exception_handler(array($handlerConfig['class'], $handlerConfig['method']));
 }
 
-if($errorConfig = \Tlc\Conf\Conf::getConfig('error_handler')) {
+if($errorConfig = Conf::getConfig('error_handler')) {
     set_error_handler(array($errorConfig['class'], $errorConfig['method']));
 }
 
-\Tlc\Bootstrap\Bootstrap::run();
+echo Bootstrap::run();
