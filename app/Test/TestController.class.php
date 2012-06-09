f2923045861f06a5d5166c21a945c3db670dd292
Guillaume Dievart <dievartg@gmail.com>
2012-06-09 17:52:07 +0200

    Move default configuration to bootstrap application

--- app/Test/TestController.class.php
+++ app/Test/TestController.class.php
@@ -3,6 +3,7 @@ namespace Test;
 
 use Telco\Controller\Controller;
 use Telco\Conf\Conf;
+use Telco\Autoloader\Autoloader;
 
 /**
  * Description of Main
@@ -12,11 +13,4 @@ use Telco\Conf\Conf;
 abstract class TestController extends Controller 
 {
     
-    /**
-     * Merge configuration app Test 
-     */
-    public function __construct()
-    {
-        Conf::mergeConfig(__DIR__.'/etc/config.ini');
-    }
 }
