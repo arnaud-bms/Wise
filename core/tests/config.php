5af42d70a0a083964f3e906e2036e65e988f8766
Guillaume Dievart <dievartg@gmail.com>
2012-06-09 18:20:53 +0200

    Default configuration for test

---
+++ core/tests/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Default configuration for UnitTests 
+ */
+
+use Telco\Autoloader\Autoloader;
+use Telco\Conf\Conf;
+
+define('ROOT_DIR', __DIR__.'/../../');
+require_once ROOT_DIR.'/core/components/Autoloader/Autoloader.class.php';
+spl_autoload_register(array('\Telco\Autoloader\Autoloader', 'loadClass'));
+Autoloader::setAlias(array(
+    'Telco'         => ROOT_DIR.'core/components',
+    'Test'          => ROOT_DIR.'app/Test',
+    'Plugin'      => ROOT_DIR.'plugins/Plugin'
+));
+
+Conf::loadConfig(ROOT_DIR.'etc/telco.ini');
+Conf::mergeConfig(ROOT_DIR.'etc/routing.ini');
