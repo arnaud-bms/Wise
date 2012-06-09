36dccb9ca37e4382fb56ecc801b1a5b95312938b
Guillaume Dievart <dievartg@gmail.com>
2012-06-09 10:15:53 +0200

    Add comment

--- app/Test/TestController.class.php
+++ app/Test/TestController.class.php
@@ -12,6 +12,9 @@ use Telco\Conf\Conf;
 abstract class TestController extends Controller 
 {
     
+    /**
+     * Merge configuration app Test 
+     */
     public function __construct()
     {
         Conf::mergeConfig(__DIR__ . '/etc/config.ini');
