fa99d5b00b235ac498ac3b3b75b294901c5b10b0
Guillaume Dievart <dievartg@gmail.com>
2012-06-09 09:48:44 +0200

    Add redirect method

--- lib/Telco/Controller/Controller.class.php
+++ lib/Telco/Controller/Controller.class.php
@@ -1,6 +1,9 @@
 <?php
 namespace Telco\Controller;
 
+use Telco\Bootstrap\Bootstrap;
+use Telco\Router\Router;
+
 /**
  * Abstract Controller
  *
@@ -9,4 +12,21 @@ namespace Telco\Controller;
 abstract class Controller 
 {
     
+    /**
+     * Redirect
+     * 
+     * @param type $route
+     * @param type $type 
+     */
+    protected function _redirect($route, $type = Router::SAPI_CGI)
+    {
+        if($type === Router::SAPI_CGI) {
+            $_SERVER['REQUEST_URI'] = $route;
+        } else {
+            echo $route . "\n";
+            $_SERVER['argv'][0] = '';
+            $_SERVER['argv'] = array_merge($_SERVER['argv'], explode(' ', $route));
+        }
+        Bootstrap::run();
+    }
 }
