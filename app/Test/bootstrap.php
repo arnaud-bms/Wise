02e5eb678355e16c85d20cba63644ab4ff5fa165
Guillaume Dievart <dievartg@gmail.com>
2012-06-13 20:28:27 +0200

    Delete alias PlugTest

--- app/Test/bootstrap.php
+++ app/Test/bootstrap.php
@@ -9,8 +9,7 @@ use Telco\Autoloader\Autoloader;
 use Telco\Conf\Conf;
 
 Autoloader::addAlias(array(
-    'Test'      => ROOT_DIR.'app/Test',
-    'PlugTest'  => ROOT_DIR.'plugins/PlugTest'
+    'Test'      => ROOT_DIR.'app/Test'
 ));
 
 Conf::mergeConfig(__DIR__.'/etc/config.ini');
