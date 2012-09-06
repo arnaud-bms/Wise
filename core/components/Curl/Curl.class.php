df38b1d4fb77d01339879d4f7e4f424cb4cbb0ea
Guillaume Dievart <dievartg@gmail.com>
2012-09-06 21:51:34 +0200

    Add extends component

--- core/components/Curl/Curl.class.php
+++ core/components/Curl/Curl.class.php
@@ -8,138 +8,138 @@ use Telco\Component\Component;
   *
   * @author gdievart <dievartg@gmail.com>
   */
-class Curl 
+class Curl extends Component
 {
 
-	/**
-	 * @var resource Handle on cUrl
-	 */
-	 private $_curl;
+    /**
+     * @var resource Handle on cUrl
+     */
+     private $_curl;
 
-	/**
-	 * @var string Last error generated
-	 */
-	private $_error = null;
+    /**
+     * @var string Last error generated
+     */
+    private $_error = null;
 
-	/**
-	 * @var int Timeout
-	 */
-	private $_timeout = 3;
+    /**
+     * @var int Timeout
+     */
+    private $_timeout = 3;
 
 
-	/**
-	 * Construct Curl
-	 *
-	 * @param string $url
-	 */
-	public function __construct($url = null) 
+    /**
+     * Construct Curl
+     *
+     * @param string $url
+     */
+    public function __construct($url = null) 
     {
-		$this->_initCurl($url);
-	}
+        $this->_initCurl($url);
+    }
 
 
-	/**
-	 * Init basic option Curl
-	 *
-	 * @param string $url
-	 */
-	private function _initCurl($url) 
+    /**
+     * Init basic option Curl
+     *
+     * @param string $url
+     */
+    private function _initCurl($url) 
     {
-		$this->_curl = curl_init();
-		$this->setUrl($url);
-		$this->setOpt(CURLOPT_TIMEOUT, $this->_timeout);
-		$this->setOpt(CURLOPT_RETURNTRANSFER, 1);
-	}
+        $this->_curl = curl_init();
+        $this->setUrl($url);
+        $this->setOpt(CURLOPT_TIMEOUT, $this->_timeout);
+        $this->setOpt(CURLOPT_RETURNTRANSFER, 1);
+    }
 
 
-	/**
-	 * Execute query
-	 *
-	 * @return mixed
-	 */
-	public function exec() 
+    /**
+     * Execute query
+     *
+     * @return mixed
+     */
+    public function exec() 
     {
-		if(($return = curl_exec($this->_curl)) === false) {
-			throw new CurlException(curl_error($this->_curl));
+        if(($return = curl_exec($this->_curl)) === false) {
+            throw new CurlException(curl_error($this->_curl));
         }
-		return $return;
-	}
+        return $return;
+    }
 
 
-	/**
-	 * Return handle on resource curl
-	 *
-	 * @return resource
-	 */
-	public function getHandle() 
+    /**
+     * Return handle on resource curl
+     *
+     * @return resource
+     */
+    public function getHandle() 
     {
-		return $this->_curl;
-	}
+        return $this->_curl;
+    }
 
 
-	/**
-	 * Return info about request
-	 *
-	 * @return array
-	 */
-	public function getInfo() 
+    /**
+     * Return info about request
+     *
+     * @return array
+     */
+    public function getInfo() 
     {
-		return curl_getinfo($this->_curl);
-	}
+        return curl_getinfo($this->_curl);
+    }
 
 
-	/**
-	 * Return last error
-	 *
-	 * @return string
-	 */
-	public function getError() 
+    /**
+     * Return last error
+     *
+     * @return string
+     */
+    public function getError() 
     {
-		return $this->_error;
-	}
+        return $this->_error;
+    }
 
 
-	/**
-	 * Set property url
-	 *
-	 * @param string $url
-	 */
-	public function setUrl($url) 
+    /**
+     * Set property url
+     *
+     * @param string $url
+     */
+    public function setUrl($url) 
     {
-		if($url !== null) {
-			$this->setOpt(CURLOPT_URL, $url);
+        if($url !== null) {
+            $this->setOpt(CURLOPT_URL, $url);
         }
-	}
+    }
 
 
-	/**
-	 * Set option curl
-	 *
-	 * @param int $option
-	 * @param mixed $value
-	 */
-	public function setOpt($option, $value) 
+    /**
+     * Set option curl
+     *
+     * @param int $option
+     * @param mixed $value
+     */
+    public function setOpt($option, $value) 
     {
-		curl_setopt($this->_curl, $option, $value);
-	}
+        curl_setopt($this->_curl, $option, $value);
+    }
 
 
-	/**
-	 * Set list options curl
-	 *
-	 * @param int $listOptions
-	 */
-	public function setOptArray(array $listOptions) 
+    /**
+     * Set list options curl
+     *
+     * @param int $listOptions
+     */
+    public function setOptArray(array $listOptions) 
     {
-		curl_setopt_array($this->_curl, $listOptions);
-	}
+        curl_setopt_array($this->_curl, $listOptions);
+    }
 
 
-	/**
-	 * Close curl
-	 */
-	public function __destruct() 
+    /**
+     * Close curl
+     */
+    public function __destruct() 
     {
-		curl_close($this->_curl);
-	}
+        curl_close($this->_curl);
+    }
 } 
--- core/components/Curl/ManageCurl.class.php
+++ core/components/Curl/ManageCurl.class.php
@@ -12,258 +12,258 @@ use Telco\Router\CurlException;
 class ManageCurl extends Component
 {
 
-	/**
-	 * @var array List ref on curl
-	 */
-	private $_listCurl = array();
+    /**
+     * @var array List ref on curl
+     */
+    private $_listCurl = array();
 
-	/**
-	 * @var array List curl to execute
-	 */
-	private $_listCurlToExecute = array();
+    /**
+     * @var array List curl to execute
+     */
+    private $_listCurlToExecute = array();
 
-	/**
-	 * @var array List response to request
-	 */
-	private $_listResponse = array();
+    /**
+     * @var array List response to request
+     */
+    private $_listResponse = array();
 
-	/**
-	 * @var boolean Auto exec query add
-	 */
-	private $_autoExec = true;
+    /**
+     * @var boolean Auto exec query add
+     */
+    private $_autoExec = true;
 
-	/**
-	 * @var int Timeout
-	 */
-	private $_timeout = 3;
+    /**
+     * @var int Timeout
+     */
+    private $_timeout = 3;
 
-	/**
-	 * List of Curl Info
-	 */
-	private $_listInfos;
+    /**
+     * List of Curl Info
+     */
+    private $_listInfos;
 
-	/**
-	 * Execute request
+    /**
+     * Execute request
      * 
-	 * @param string $url
-	 */
-	public function addRequest($url) 
+     * @param string $url
+     */
+    public function addRequest($url) 
     {
-		return $this->addCurl(new Curl($url));
-	}
+        return $this->addCurl(new Curl($url));
+    }
 
 
-	/**
-	 * Execute multi_exec
-	 */
-	public function request() 
+    /**
+     * Execute multi_exec
+     */
+    public function request() 
     {
-		$multiHandle = $this->_initMultiHandle();
-		$this->_execMultiHandle($multiHandle);
-		$this->_readResponseRequest($multiHandle);
-	}
+        $multiHandle = $this->_initMultiHandle();
+        $this->_execMultiHandle($multiHandle);
+        $this->_readResponseRequest($multiHandle);
+    }
 
 
-	/**
-	 * Add Curl to execute on multi
+    /**
+     * Add Curl to execute on multi
      * 
-	 * @param Curl $curl
-	 */
-	public function addCurl(Curl $curl)
+     * @param Curl $curl
+     */
+    public function addCurl(Curl $curl)
     {
-		$uniqId = uniqid();
-		$this->_listCurl[$uniqId] = $curl;
-		$this->_execCurl($uniqId);
-		return $uniqId;
-	}
+        $uniqId = uniqid();
+        $this->_listCurl[$uniqId] = $curl;
+        $this->_execCurl($uniqId);
+        return $uniqId;
+    }
 
 
-	/**
-	 * Init multi_handle
+    /**
+     * Init multi_handle
      * 
-	 * @return resource
-	 */
-	private function _initMultiHandle() 
+     * @return resource
+     */
+    private function _initMultiHandle() 
     {
-		$multiHandle = curl_multi_init();
-		foreach($this->_listCurlToExecute as $uniqId) {
-			$curl = $this->_getCurl($uniqId);
-			curl_multi_add_handle($multiHandle, $curl->getHandle());
-		}
-		return $multiHandle;
-	}
+        $multiHandle = curl_multi_init();
+        foreach($this->_listCurlToExecute as $uniqId) {
+            $curl = $this->_getCurl($uniqId);
+            curl_multi_add_handle($multiHandle, $curl->getHandle());
+        }
+        return $multiHandle;
+    }
 
 
-	/**
-	 * Execute curl in multi
+    /**
+     * Execute curl in multi
      * 
-	 * @param resource $multiHandle
-	 */
-	private function _execMultiHandle($multiHandle) 
+     * @param resource $multiHandle
+     */
+    private function _execMultiHandle($multiHandle) 
     {
-		$running = 0;
-		$startTime = microtime(true);
-		do {
-			curl_multi_exec($multiHandle, $running );
-		} while ($running != 0 && (microtime(true) < $startTime + $this->_timeout));
-	}
+        $running = 0;
+        $startTime = microtime(true);
+        do {
+            curl_multi_exec($multiHandle, $running );
+        } while ($running != 0 && (microtime(true) < $startTime + $this->_timeout));
+    }
 
 
-	/**
-	 * Read response to request
+    /**
+     * Read response to request
      * 
-	 * @param resource $multiHandle
-	 */
-	private function _readResponseRequest($multiHandle)
+     * @param resource $multiHandle
+     */
+    private function _readResponseRequest($multiHandle)
     {
-		while($curlDone = curl_multi_info_read($multiHandle, $nbMsg = 0)) {
-			if(curl_errno($curlDone['handle']) == CURLE_OK) {
-				foreach($this->_listCurlToExecute as $key => $uniqId) {
-					$curl = $this->_getCurl($uniqId);
-					if ($curl->getHandle() === $curlDone['handle']) {
-						$this->_addToCallback($curl, $uniqId);
-						curl_multi_remove_handle($multiHandle, $curl->getHandle());
-						unset($this->_listCurlToExecute[$key]);
-					}
-				}
-			}
-		}
-	}
+        while($curlDone = curl_multi_info_read($multiHandle, $nbMsg = 0)) {
+            if(curl_errno($curlDone['handle']) == CURLE_OK) {
+                foreach($this->_listCurlToExecute as $key => $uniqId) {
+                    $curl = $this->_getCurl($uniqId);
+                    if ($curl->getHandle() === $curlDone['handle']) {
+                        $this->_addToCallback($curl, $uniqId);
+                        curl_multi_remove_handle($multiHandle, $curl->getHandle());
+                        unset($this->_listCurlToExecute[$key]);
+                    }
+                }
+            }
+        }
+    }
 
 
-	/**
-	 * Method called in call back
-	 * 
-	 * @param string $content
-	 * @param string $uniqId
-	 * @param array $infos
-	 */
-	private function _callBack($content, $uniqId, array $infos) 
+    /**
+     * Method called in call back
+     * 
+     * @param string $content
+     * @param string $uniqId
+     * @param array $infos
+     */
+    private function _callBack($content, $uniqId, array $infos) 
     {
-		$this->_listInfos[$uniqId] = $infos;
-		$this->_listResponse[$uniqId] = $content;
-	}
+        $this->_listInfos[$uniqId] = $infos;
+        $this->_listResponse[$uniqId] = $content;
+    }
 
 
-	/**
-	 * Add callback for handle
-	 * 
-	 * @param Curl $curl
-	 * @param string $uniqId
-	 */
-	private function _addToCallback(Curl $curl, $uniqId) 
+    /**
+     * Add callback for handle
+     * 
+     * @param Curl $curl
+     * @param string $uniqId
+     */
+    private function _addToCallback(Curl $curl, $uniqId) 
     {
-		call_user_func(
+        call_user_func(
                 array($this, '_callBack'), 
                 curl_multi_getcontent($curl->getHandle()), 
                 $uniqId, 
                 $curl->getInfo());
-	}
+    }
 
 
-	/**
-	 * Execute request Curl
-	 * 
-	 * @param string $uniqId
-	 */
-	private function _execCurl($uniqId) 
+    /**
+     * Execute request Curl
+     * 
+     * @param string $uniqId
+     */
+    private function _execCurl($uniqId) 
     {
-		$curl = $this->_getCurl($uniqId);
-		if($this->_autoExec) {
-			$this->_listResponse[$uniqId] = $curl->exec();
+        $curl = $this->_getCurl($uniqId);
+        if($this->_autoExec) {
+            $this->_listResponse[$uniqId] = $curl->exec();
         } else {
-			$this->_listCurlToExecute[] = $uniqId;
+            $this->_listCurlToExecute[] = $uniqId;
         }
-	}
+    }
 
 
-	/**
-	 * Return response to request
-	 * 
-	 * @param string $uniqId
-	 * @return string
-	 */
-	public function getResponse($uniqId) 
+    /**
+     * Return response to request
+     * 
+     * @param string $uniqId
+     * @return string
+     */
+    public function getResponse($uniqId) 
     {
-		if(array_key_exists($uniqId, $this->_listResponse)) {
-			return $this->_listResponse[$uniqId];
+        if(array_key_exists($uniqId, $this->_listResponse)) {
+            return $this->_listResponse[$uniqId];
         }
-	}
+    }
 
 
-	/**
-	 * Return informations about request
-	 * 
-	 * @param string $uniqId
-	 * @return array
-	 */
-	public function getInfos($uniqId) 
+    /**
+     * Return informations about request
+     * 
+     * @param string $uniqId
+     * @return array
+     */
+    public function getInfos($uniqId) 
     {
-		return $this->_listInfos[$uniqId];
-	}
+        return $this->_listInfos[$uniqId];
+    }
 
 
-	/**
-	 * Set option to Curl
-	 * 
-	 * @param string $uniqId
-	 * @param int $option
-	 * @param mixed $value
-	 */
-	public function setOpt($uniqId, $option, $value) 
+    /**
+     * Set option to Curl
+     * 
+     * @param string $uniqId
+     * @param int $option
+     * @param mixed $value
+     */
+    public function setOpt($uniqId, $option, $value) 
     {
-		$curl = $this->_getCurl($uniqId);
-		$curl->setOpt($option, $value);
-	}
+        $curl = $this->_getCurl($uniqId);
+        $curl->setOpt($option, $value);
+    }
 
 
-	/**
-	 * Set list options to Curl
-	 * 
-	 * @param string $uniqId
-	 * @param array $listOptions
-	 */
-	public function setOptArray($uniqId, array $listOptions) 
+    /**
+     * Set list options to Curl
+     * 
+     * @param string $uniqId
+     * @param array $listOptions
+     */
+    public function setOptArray($uniqId, array $listOptions) 
     {
-		$curl = $this->_getCurl($uniqId);
-		$curl->setOptArray($listOptions);
-	}
+        $curl = $this->_getCurl($uniqId);
+        $curl->setOptArray($listOptions);
+    }
 
 
-	/**
-	 * Set auto exec
-	 * 
-	 * @param boolean $autoExec
-	 */
-	public function setAutoExec($autoExec) 
+    /**
+     * Set auto exec
+     * 
+     * @param boolean $autoExec
+     */
+    public function setAutoExec($autoExec) 
    {
-		$this->_autoExec = $autoExec;
-	}
+        $this->_autoExec = $autoExec;
+    }
 
 
-	/**
-	 * Set timeout
+    /**
+     * Set timeout
      * 
-	 * @param int $second
-	 */
-	public function setTimeout($second) 
+     * @param int $second
+     */
+    public function setTimeout($second) 
     {
-		$this->_timeout = $second;
-	}
+        $this->_timeout = $second;
+    }
 
 
-	/**
-	 * Return ref on Curl
+    /**
+     * Return ref on Curl
      * 
-	 * @param string $uniqId
-	 * @return Curl
-	 */
-	private function _getCurl($uniqId) 
+     * @param string $uniqId
+     * @return Curl
+     */
+    private function _getCurl($uniqId) 
     {
-		if(array_key_exists($uniqId, $this->_listCurl)) {
-			return $this->_listCurl[$uniqId];
+        if(array_key_exists($uniqId, $this->_listCurl)) {
+            return $this->_listCurl[$uniqId];
         } else {
             throw new CurlException('Uniq id doesn\'t exists');
         }
-	}
+    }
 }
