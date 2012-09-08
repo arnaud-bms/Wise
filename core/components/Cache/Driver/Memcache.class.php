3b073ec714a9745bc30e93de83d651333d6768db
Guillaume Dievart <dievartg@gmail.com>
2012-09-08 22:40:13 +0200

    Wrong args to add method

--- core/components/Cache/Driver/Memcache.class.php
+++ core/components/Cache/Driver/Memcache.class.php
@@ -10,28 +10,28 @@ use Telco\Cache\Driver\Driver;
  */
 class Memcache extends Driver
 {
-    
+
     /**
      * @var MemCache
      */
     protected $_memcache;
-    
+
     /**
      * @var int ttl
      */
     protected $_ttl;
-    
+
     /**
-     * @var array Required fields 
+     * @var array Required fields
      */
     protected $_requiredFields = array(
         'host',
         'port',
         'ttl'
     );
-    
+
     /**
-     * 
+     *
      * @param type $config
      */
     protected function _init($config)
@@ -40,26 +40,26 @@ class Memcache extends Driver
         $this->_memcache->connect($config['host'], $config['port']);
         $this->_ttl = $config['ttl'];
     }
-    
+
     /**
      * Retrieve valid cache
-     * 
-     * @param type $uniqId 
+     *
+     * @param type $uniqId
      */
     public function getCache($uniqId)
     {
         return $this->_memcache->get($uniqId);
     }
-    
-    
+
+
     /**
      * Set cache
-     * 
+     *
      * @param type $uniqId
-     * @param type $content 
+     * @param type $content
      */
     public function setCache($uniqId, $content)
     {
-        $this->_memcache->add($uniqId, $content, $this->_ttl);
+        $this->_memcache->add($uniqId, $content, false, $this->_ttl);
     }
 }
