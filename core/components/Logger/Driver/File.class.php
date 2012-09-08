6243f62f9eaf760f53a99053fe0d9fcf1c98000b
Guillaume Dievart <dievartg@gmail.com>
2012-09-08 22:38:15 +0200

    Change AbstractDriver to Driver

--- core/components/Logger/Driver/File.class.php
+++ core/components/Logger/Driver/File.class.php
@@ -1,47 +1,46 @@
 <?php
 namespace Telco\Logger\Driver;
 
-use Telco\Logger\Driver\AbstractDriver;
-use Telco\Logger\LoggerException;
+use Telco\Logger\Driver\Driver;
 
 /**
  * File driver for Logger
  *
  * @author gdievart <dievartg@gmail.com>
  */
-class File extends AbstractDriver
+class File extends Driver
 {
     /**
-     * @var array Required fields 
+     * @var array Required fields
      */
     protected $_requiredFields = array(
         'file'
     );
-    
+
     /**
-     * @var string Path to cache folder 
+     * @var string Path to cache folder
      */
     protected $_file;
-    
+
     /**
-     * @var handle 
+     * @var handle
      */
     protected $_handle;
-    
+
     /**
      * Init File driver
-     * 
-     * @param array $config 
+     *
+     * @param array $config
      */
     protected function _init($config)
     {
         $this->_file = $config['file'];
     }
-    
-    
+
+
     /**
      * Retrieve valid cache
-     * 
+     *
      * @param string $message
      * @param int  $level
      */
@@ -50,8 +49,8 @@ class File extends AbstractDriver
         $this->_initHandle();
         fwrite($this->_handle, $message);
     }
-    
-    
+
+
     /**
      * Init handle
      */
@@ -61,8 +60,8 @@ class File extends AbstractDriver
             $this->_handle = fopen($this->_file, 'a+');
         }
     }
-    
-    
+
+
     /**
      * Close file
      */
