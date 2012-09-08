ed354a3d670577b3c79ba41f0287e4e2e831ceeb
Guillaume Dievart <dievartg@gmail.com>
2012-09-08 22:39:05 +0200

    Extends Component

--- core/components/Entity/Entity.class.php
+++ core/components/Entity/Entity.class.php
@@ -1,12 +1,14 @@
 <?php
 namespace Telco\Entity;
 
+use Telco\Component\Component;
+
 /**
  * Entity
  *
  * @author gdievart <dievartg@gmail.com>
  */
-abstract class Entity
+abstract class Entity extends Component
 {
     
 }
