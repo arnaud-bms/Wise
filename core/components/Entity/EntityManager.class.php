7f6d68f45926a7e019e5dcb391fa083ac8e386ed
Guillaume Dievart <g.dievart@telemaque.fr>
2012-11-28 15:46:51 +0100

    class to manage multiple entity

---
+++ core/components/Entity/EntityManager.class.php
@@ -0,0 +1,60 @@
+<?php
+namespace Telelab\Entity;
+
+use Telelab\Component\Component;
+use Telelab\SqlBuilder\SqlBuilder;
+
+/**
+ * Entity
+ *
+ * @author gdievart <dievartg@gmail.com>
+ */
+class EntityManager extends Component
+{
+    /**
+     * @var array Ref to entity
+     */
+    private $_entities = array();
+
+    /**
+     * @var SqlBuilder
+     */
+    private $_sqlBuilder;
+
+    /**
+     * Init members of DAO
+     *
+     * @param array $row
+     */
+    protected function _init($row)
+    {
+        $this->_hydrate($row);
+        $this->_sqlBuilder = new SqlBuilder($this->_tableName);
+    }
+
+
+    /**
+     * Create entity
+     *
+     * @param string $entityName
+     * @return Entity
+     */
+    public function createEntity($entityName)
+    {
+        $entity = new $entityName;
+        $this->_entities[$entityName][] = $entity;
+
+        return $entity;
+    }
+
+
+    /**
+     * Save all entities
+     */
+    public function save()
+    {
+        /**
+         * @todo Get all entities from array and do insert multique
+         */
+    }
+}
