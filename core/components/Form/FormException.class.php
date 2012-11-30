df5c7de117cd76aca47c17db2189ae678cef774b
Guillaume Dievart <g.dievart@telemaque.fr>
2012-11-30 17:19:58 +0100

    first commit

---
+++ core/components/Form/FormException.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Telelab\Form;
+
+use Telelab\Component\ComponentException;
+
+/**
+ * FormException
+ *
+ * @author gdievart <g.dievart@telemaque.fr>
+ */
+class FormException extends ComponentException
+{
+
+}
