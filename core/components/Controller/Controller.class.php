4ef0b8d74fe06a048b9b6a25215d05d3258a179f
Florent Manas <florent@telemaque.fr>
2013-01-14 12:32:35 +0100

    controller: add server support methods

--- core/components/Controller/Controller.class.php
+++ core/components/Controller/Controller.class.php
@@ -3,6 +3,7 @@ namespace Telelab\Controller;
 
 use Telelab\FrontController\FrontController;
 use Telelab\Component\Component;
+use Telelab\Server\Server;
 
 /**
  * Controller: Base class for controller
@@ -16,6 +17,22 @@ abstract class Controller extends Component
      * @var array Repository loaded
      */
     protected $_repositoryLoaded = array();
+
+    /**
+     * @var Server instance
+     */
+    private static $_server = null;
+
+
+    /**
+     * Init Controller
+     *
+     * @param array $config
+     */
+    protected function _init($config)
+    {
+        self::$_server = new Server();
+    }
 
     /**
      * Redirect
@@ -42,5 +59,22 @@ abstract class Controller extends Component
         }
 
         return $this->_repositoryLoaded[$repository];
+    }
+
+
+    /**
+     * Call method on server
+     *
+     * @param string $method
+     * @param array $argv
+     * @return mixed
+     */
+    public function __call($method, $argv)
+    {
+        if (method_exists(self::$_server, $method)) {
+            return call_user_func_array(array(self::$_server, $method), $argv);
+        }
+
+        return null;
     }
 }
