516b45f92f0e4502030624fcea884a6b6d76f023
Florent Manas <florent@telemaque.fr>
2013-01-24 10:41:05 +0100

    arraytype: merge array recursive

---
+++ core/components/Type/ArrayType.class.php
@@ -0,0 +1,31 @@
+<?php
+namespace Telelab\Type;
+
+use Telelab\Component\Component;
+
+/**
+ * FormHelper: Helper that generate form components (html) and handle it
+ *
+ * @author fmanas <f.manas@telemaque.fr>
+ */
+
+class ArrayType extends Component
+{
+
+    /**
+     * Merge array recursive override (php native array_merge_recursive merge creating array)
+     *
+     * @param array $firstArray
+     * @param array $secondArray
+     * @return array
+     */  
+    public static function mergeRecursive($firstArray, $secondArray)
+    {
+        if (!is_array($firstArray) or !is_array($secondArray)) { return $secondArray; }
+        foreach ($secondArray AS $sKey2 => $sValue2)
+        {
+            $firstArray[$sKey2] = self::mergeRecursive(@$firstArray[$sKey2], $sValue2);
+        }
+        return $firstArray;
+    } 
+}
