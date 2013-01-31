b68cdaf3767c7051310b59385a3e9a184311c110
Guillaume Dievart <g.dievart@telemaque.fr>
2013-01-31 16:28:42 +0100

    TranslatorHelper: First commit

---
+++ core/components/Translation/TranslatorHelper.class.php
@@ -0,0 +1,40 @@
+<?php
+namespace Telelab\Translation;
+
+use Telelab\Component\Component;
+
+/**
+ * TranslatorHelper: i18n & l10n
+ *
+ * @author fmanas <f.manas@telemaque.fr>
+ */
+class TranslatorHelper extends Component
+{
+
+    /**
+     * Constructor
+     *
+     * @param array $config
+     */
+    protected function _init($config)
+    {
+
+    }
+
+
+    /**
+     * Get instance Translator
+     *
+     * @param string $lang
+     * @return Translator
+     */
+    public static function handle($lang)
+    {
+        $html = '<script type="text/javascript" src="/js/telelab/telelab.lang.js"></script>'
+              . '<script type="text/javascript">'
+              . 'lang.load('.$lang.')'
+              . '</script>';
+
+        return $html;
+    }
+}
