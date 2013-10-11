e96ab6228ac15fdaca9d10ed511eada3db253b16
GuillaumeDievart <dievartg@gmail.com>
2013-10-11 10:10:17 +0200

    Tests: remove Conf from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,5 @@
  * Default configuration for UnitTests
  */
 
-use Telelab\Conf\Conf;
-
 define('ROOT_DIR', __DIR__.'/../');
 require_once ROOT_DIR.'/vendor/autoload.php';
-
-Conf::loadConfig(ROOT_DIR.'app/etc/app.ini');
-Conf::mergeConfig(ROOT_DIR.'app/etc/routing.ini');
