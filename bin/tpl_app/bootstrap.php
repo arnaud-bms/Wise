606cf7e14ed7a86a553d0a93d481442580ed5c81
Guillaume Dievart <dievartg@gmail.com>
2013-11-08 21:34:22 +0100

    config: change the configuration ini to php

--- bin/tpl_app/bootstrap.php
+++ bin/tpl_app/bootstrap.php
@@ -10,6 +10,6 @@ use Telelab\Conf\Conf;
 $loader = require ROOT_DIR.'/vendor/autoload.php';
 $loader->add('{{app_name}}', ROOT_DIR.'/app');
 
-Conf::mergeConfig(__DIR__.'/etc/config.ini');
-Conf::mergeConfig(__DIR__.'/etc/routing.ini');
-Conf::mergeConfig(__DIR__.'/etc/view.ini');
+Conf::mergeConfig(__DIR__.'/etc/config.php');
+Conf::mergeConfig(__DIR__.'/etc/routing.php');
+Conf::mergeConfig(__DIR__.'/etc/view.php');
