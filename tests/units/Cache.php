d0854c8f6b27a78e06efc392ee4afaec80974454
GuillaumeDievart <dievartg@gmail.com>
2013-11-19 14:52:35 +0100

    tests: added a mock to test the Cache component

--- tests/units/Cache.php
+++ tests/units/Cache.php
@@ -10,142 +10,71 @@ use atoum;
  */
 class Cache extends atoum
 {
-    
     private $cache;
     
-    private function getCache($driver = 'memcache', $enable = true)
+    private function getCache()
     {
         if ($this->cache === null) {
+            $this->mockGenerator->generate('\Wise\Cache\Driver\Cache', '\Wise\Cache\Driver', 'Mock')
+                                ->overload(new atoum\mock\php\method('__construct'));
 
-            switch($driver) {
-                case 'memcache':
-                    $config = array(
-                        'enable' => $enable,
-                        'driver' => 'memcache',
-                        'memcache' => array('ttl' => 10, 'host' => '127.0.0.1', 'port' => 11211)
-                    );
-                    break;
-                case 'file':
-                    $config = array(
-                        'enable' => $enable,
-                        'driver' => 'file',
-                        'file' => array('ttl' => 600, 'path' => '/tmp/cache')
-                    );
-                    break;
-                case 'fake':
-                    $config = array(
-                        'enable' => $enable,
-                        'driver' => 'fake'
-                    );
-                    break;
-            }
+            $controller = new \atoum\mock\controller();
+            $controller->__construct = function() { $this->cache = array(); };
+            $controller->set         = function($key, $value) { $this->cache[$key] = $value; };
+            $controller->get         = function($key) { return (isset($this->cache[$key])) ? $this->cache[$key] : false ;};
+            $controller->delete      = function($key) { unset($this->cache[$key]);};
+            $controller->flush       = function() { $this->cache = array(); };
+            $controller->increment   = function($key) { $this->cache[$key]++; };
+            $controller->decrement   = function($key) {  $this->cache[$key]--; };
+
+            $config = array(
+                'enable' => true,
+                'driver' => 'mock',
+                'mock'   => $controller
+            );
+
             $this->cache = new \Wise\Cache\Cache($config);
         }
         
         return $this->cache;
     }
     
-    public function _testFlushWithFileDriver()
+    public function testFlush()
     {
-        $this->testIncrement('file');
-    }
-    
-    public function _testFlushWithMemcacheDriver()
-    {
-        $this->testIncrement('memcache');
-    }
-    
-    protected function _testFlush($driver)
-    {
-        $this->getCache($driver)->set('flush', 5);
-        $this->getCache($driver)->flush();
-        $this->assert->boolean($this->getCache($driver)->get('flush'))
+        $this->getCache()->set('flush', 5);
+        $this->getCache()->flush();
+        $this->assert->boolean($this->getCache()->get('flush'))
                      ->isFalse(6);
     }
-
-    public function _testGetDisable()
+    
+    public function testGet()
     {
-        $this->getCache('memcache', false)->set('disable', 'content');
-        $this->assert->boolean($this->getCache('memcache', false)->get('disable'))
-            ->isFalse();
+        $this->getCache()->set('get', 'content');
+        $this->assert->string($this->getCache()->get('get'))
+                     ->isEqualTo('content');
     }
     
-    public function _testGetWithMemcacheDriver()
+    public function testDelete()
     {
-        $this->testGet('memcache');
-    }
-
-    public function _testGetWithFileDriver()
-    {
-        $this->testGet('file');
-    }
-    
-    protected function _testGet($driver)
-    {
-        $this->getCache($driver)->set('get', 'content');
-        $this->assert->string($this->getCache($driver)->get('get'))
-            ->isEqualTo('content');
-    }
-    
-    public function _testgetWithFakeDriver()
-    {
-        $this->exception(function() {
-            $this->getCache('fake');
-        })
-            ->isInstanceOf('\Wise\Cache\Exception');
-    }
-    
-    public function _testDeleteWithFileDriver()
-    {
-        $this->testDelete('file');
-    }
-    
-    public function _testDeleteWithMemcacheDriver()
-    {
-        $this->testDelete('memcache');
-    }
-    
-    protected function _testDelete($driver)
-    {
-        $this->getCache($driver)->set('delete', 'content');
-        $this->getCache($driver)->delete('delete');
-        $this->assert->boolean($this->getCache($driver)->get('delete'))
+        $this->getCache()->set('delete', 'content');
+        $this->getCache()->delete('delete');
+        $this->assert->boolean($this->getCache()->get('delete'))
                      ->isFalse();
     }
     
-    public function _testDecrementWithFileDriver()
+    public function testDecrement()
     {
-        $this->testDecrement('file');
-    }
-    
-    public function _testDecrementWithMemcacheDriver()
-    {
-        $this->testDecrement('memcache');
-    }
-    
-    protected function _testDecrement($driver)
-    {
-        $this->getCache($driver)->set('decrement', 5);
-        $this->getCache($driver)->decrement('decrement');
-        $this->assert->integer((int)$this->getCache($driver)->get('decrement'))
+        $this->getCache()->set('decrement', 5);
+        $this->getCache()->decrement('decrement');
+        $this->assert->integer((int)$this->getCache()->get('decrement'))
                      ->isEqualTo(4);
     }
     
-    public function _testIncrementWithFileDriver()
+    public function testIncrement()
     {
-        $this->testIncrement('file');
-    }
-    
-    public function _testIncrementWithMemcacheDriver()
-    {
-        $this->testIncrement('memcache');
-    }
-    
-    protected function _testIncrement($driver)
-    {
-        $this->getCache($driver)->set('increment', 5);
-        $this->getCache($driver)->increment('increment');
-        $this->assert->integer((int)$this->getCache($driver)->get('increment'))
+        $this->getCache()->set('increment', 5);
+        $this->getCache()->increment('increment');
+        $this->assert->integer((int)$this->getCache()->get('increment'))
                      ->isEqualTo(6);
     }
 }
